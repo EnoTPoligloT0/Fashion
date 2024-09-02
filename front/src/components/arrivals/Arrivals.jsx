8e9ab4744b31dd84e43601a80fc870baec4a07f7
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-09-02 07:05:17 +0200

    Props integration

--- front/src/components/arrivals/Arrivals.jsx
+++ front/src/components/arrivals/Arrivals.jsx
@@ -1,4 +1,9 @@
 import styles from './Arrivals.module.css';
+import cat01Img from "../../img/categories/cat-1.png"
+import cat02Img from "../../img/categories/cat-2.png"
+import cat03Img from "../../img/categories/cat-3.png"
+import Card from "../Card/Card.jsx";
+
 function Arrivals() {
     return (
         <section className={styles.arrivals}>
@@ -7,9 +12,9 @@ function Arrivals() {
                     <h2 className={styles.title_2}>NEW ARRIVALS</h2>
                 </div>
                 <div className={styles.arrivals_cards}>
-                    <div>CARD 1</div>
-                    <div>CARD 2</div>
-                    <div>CARD 3</div>
+                    <div><Card title={"Hoodies & Sweatshirt"} img={cat01Img} /></div>
+                    <div><Card title={"Coats & Parkas"} img={cat02Img}/></div>
+                    <div><Card title={"Tees & T-Shirt"} img={cat03Img}/></div>
                 </div>
             </div>
         </section>
